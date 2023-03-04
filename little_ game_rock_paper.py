e43cc2b925a9ccce1d72d34ec4811e3136fa91d9
Andrianandraina Haja Michaël <75771234+Hagie94@users.noreply.github.com>
2023-03-04 15:35:34 +0300

    Add files via upload
    a CLI game

---
+++ little_ game_rock_paper.py
@@ -0,0 +1,41 @@
+import random
+
+def get_choices():
+    player_choice = input("Entrer votre choix [ pierre,papier,ciseaux ] : ")
+    choix_ordi = ["pierre","papier","ciseaux"]
+    computer_choice = random.choice(choix_ordi)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+def check_win(player, computer):
+    print(f"Vous avez choisi {player} ,l'ordinateur a choisi {computer}")
+    if player == computer:
+        return "egaliter"
+    elif player == "pierre":
+        if computer == "siceaux":
+            return "La pierre detruit le siceaux: Vous avez gagner"
+        else:
+            return "Le papier enveloppe le pierre: Vous avez perdu"
+    elif player == "papier":
+        if computer == "pierre":
+            return "Le papier enveloppe la pierre: Vous avez gagner"
+        else:
+            return "Le ciseaux coupe le papier: Vous avez perdu"
+    elif player == "ciseaux":
+        if computer == "papier":
+            return "Le ciseaux coupe le papier: Vous avez gagner"
+        else:
+            return "La pierre detruit le ciseaux: Vous avez perdu"
+#renvoie une dictionnaire
+choices = get_choices()
+
+#recuperation des elements de la dico
+p_choix = choices["player"]
+c_choix = choices["computer"]
+
+#insertion des valeurs dans la fonction check_win
+result = check_win(p_choix,c_choix)
+
+#Affichage du gagnant
+print(result)
+
